ed452d81fbd131cdbaaedf5bb32f3c7a7e71df0e
Elyssa Guajardo <101149603+nyoomitsellie@users.noreply.github.com>
2025-02-01 18:54:14 +0000

    finished

---
+++ src/MixMatch.java
@@ -0,0 +1,24 @@
+public class MixMatch {
+
+    // your main() method goes here
+    public static void main(String[] args) {
+        String name = "Elyssa";
+        System.out.println(name + " <- String prints out a word. ex: my name.");
+        char last = 'G';
+        System.out.println(last + " <- char prints out a singular letter. ex: my last initial");
+        short num1 = 22;
+        System.out.println(num1 + " <- short is used for integers that are 2 digits. ex: my age");
+        int num2 = 2000;
+        System.out.println(num2 + " <- int is used for regular integers");
+        long num3 = 20000000;
+        System.out.println(num3 + " <- long is used for integers that are more digits/bytes than int");
+        float num4 = (float) 2.22;
+        System.out.println(num4 + " <- float is used for integers that are decimals");
+        double num5 = 2.5837426;
+        System.out.println(num5 + " <- double is used just like float but with more numbers behind the decimal point");
+        boolean isTrue = true;
+        System.out.println(isTrue + " <- boolean is used for true and false statements");
+
+    }
+
+}
